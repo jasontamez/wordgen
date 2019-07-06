dd18ac394f9cfce5c1990257f9c893b46bcb95ba
Jason Tamez <jasontamez@users.noreply.github.com>
2019-07-06 01:35:14 -0500

    style updates
    - jQuery dependency
    * changed some variable names to be more descriptive

--- help-popups.js
+++ help-popups.js
@@ -1,39 +1,44 @@
 // Set counter for tooltips, thus giving each a unique ID and an ever-increasing z-index.
 var zCounter = 1;
 // Set up help tooltips as needed.
-$(".help").click(function() {
-	var info = $(this).find("span.info"), leftOffset, infoWidth, windowWidth, leftPosition, offsetObject, helpHTML, tooltipID;
-	if($(this).hasClass("popOut")) {
+document.querySelectorAll(".help").forEach( help => help.addEventListener("click", doHelpPopUp.bind(help)) );
+function doHelpPopUp() {
+	//console.log("clicked");
+	var info = this.querySelector("span.info"), d = this.dataset, leftOffset, infoWidth, windowWidth, leftPosition, helpHTML, tooltipID;
+	if(this.classList.contains("popOut")) {
 		//
 		// Handle tooltips from the Advanced Options.
 		//
-		tooltipID = $(this).attr("data-clicked");
-		if(typeof tooltipID !== "undefined") {
+		tooltipID = d.clicked;
+		if(tooltipID !== undefined) {
 			// data-clicked is defined? Then we've been clicked. Remove the tooltip.
-			$("#" + tooltipID).remove();
+			document.getElementById(tooltipID).remove();
 			// Remove the stored info.
-			$(this).removeAttr("data-clicked");
+			delete d.clicked;
 		} else {
 			// data-clicked isn't defined? Then create a tooltip.
 			// We can't simply show the tooltip box, since it's constrained by the flex container.
-			offsetObject = $(this).offset();
-			helpHTML = info.html();
+			helpHTML = info.innerHTML;
 			// Create an ID to remember this by, using the ever-increased zCounter.
 			++zCounter;
 			tooltipID = "popup" + zCounter;
 			// Store that ID on the question mark.
-			$(this).attr("data-clicked", tooltipID);
+			d.clicked = tooltipID;
 			// Create new tooltip node.
-			$("body").append("<div class=\"info\" id=\"" + tooltipID + "\"></div>");
+			let x = document.createElement("div");
+			x.classList.add("info");
+			x.setAttribute("id", tooltipID);
+			document.body.append(x);
 			// Select the new node.
-			info = $("#" + tooltipID);
+			info = document.getElementById(tooltipID);
 			// Make it visible.
-			info.toggle();
+			info.classList.add("shown");
 			// Check to see if we're going to go off the edge of the window.
-			leftOffset = offsetObject.left;
-			infoWidth = info.width() + 20;			// Width of info box (plus padding)
-			windowWidth = $(document).width();			// Window width
-			leftPosition = leftOffset + $(this).width();	// Starting left position
+			leftOffset = this.offsetLeft;
+			//console.log(this.offsetParent);
+			infoWidth = info.scrollWidth + 20;			// Width of info box (plus padding)
+			windowWidth = window.innerWidth;			// Window width
+			leftPosition = leftOffset + this.scrollWidth;	// Starting left position
 			if((leftPosition + infoWidth) > windowWidth) {
 				// Close to right edge of window.
 				if(leftPosition <= infoWidth) {
@@ -50,58 +55,66 @@ $(".help").click(function() {
 					leftOffset -= infoWidth;
 				}
 			}
-			info.html(helpHTML).css({"z-index": zCounter, "top": offsetObject.top + 10, "left": leftOffset} );
-			info.click(function() {
-				// Delete the stored info from the parent question mark.
-				$('span[data-clicked="' + $(this).attr("id") + '"]').removeAttr("data-clicked");
-				// Remove this.
-				$(this).remove();
+			info.innerHTML = helpHTML;
+			let s = info.style, newTop = this.offsetTop + this.scrollHeight;
+			s.position = "absolute";
+			s.top = newTop.toString() + "px";
+			s.left = leftOffset.toString() + "px";
+			s.zIndex = zCounter.toString();
+			info.addEventListener("click", function(e) {
+				var me = e.currentTarget, z = document.querySelector("span[data-clicked=\"" + me.getAttribute("id") + "\"]");
+				delete z.dataset.clicked;
+				me.remove();
 			});
+
 		}
-	} else {
+	} else if(info.classList.contains("shown")) {
 		//
 		// Handle tooltips in main body
 		//
-		// Start by toggling the info's visiblity.
-		info.toggle();
+		// This one has been shown and now needs to be hidden.
+		info.classList.remove("shown");
+	} else {
+		// Make it visible.
+		info.classList.add("shown");
 		// Transform it back to 0 (default state) just in case it was moved before.
-		info.css("transform", "translateX(0px)");
-		// We only need to calculate stuff if we're visible.
-		if(info.is(":visible")) {
-			// Increment the zCounter.
-			++zCounter;
-			// Give the info box that z-index.
-			info.css("z-index", zCounter);
-			infoWidth = info.width() + 20;				// Width of info box (plus padding)
-			windowWidth = $(document).width();				// Window width
-			offsetObject = $(this).offset();
-			leftPosition = offsetObject.left + $(this).width();	// Starting left position
-			if((leftPosition + infoWidth) > windowWidth) {
-				// Info box is close to the right edge of window.
-				if(leftPosition <= infoWidth) {
-					// Close to the left edge of window, too!
-					// Calculate best fit.
-					if(windowWidth <= infoWidth) {
-						// WHY would your window be so small??
-						// Push it to the left edge. Hope we can scroll.
-						//	0 - (leftPosition - 2)
-						//	= 0 - leftPosition + 2
-						//	= 2 - leftPosition
-						info.css("transform", "translateX(" + (2 - leftPosition) + "px)");
-					} else {
-						// Moving it left 'infoWidth' pushes it off the left edge.
-						// Leaving it pushes it off the right edge by '(windowWidth - (leftPosition + infoWidth))' pixels.
-						// So move it leftward '(windowWidth - (leftPosition + infoWidth))' and add a bit of padding
-						// Should leave it just off the right edge!
-						//	windowWidth - (leftPosition + infoWidth)
-						//	= windowWidth - leftPosition - infoWidth
-						info.css("transform", "translateX(" + (windowWidth - leftPosition - infoWidth) + "px)");
-					}
+		let s = info.style;
+		s.transform = "translateX(0px)";
+		// Increment the zCounter.
+		++zCounter;
+		// Give the info box that z-index.
+		s.position = "absolute";
+		s.zIndex = zCounter.toString();
+		infoWidth = info.scrollWidth + 20;				// Width of info box (plus padding)
+		windowWidth = window.innerWidth;				// Window width
+		leftPosition = this.offsetLeft + this.scrollWidth;	// Starting left position
+		if((leftPosition + infoWidth) > windowWidth) {
+			// Info box is close to the right edge of window.
+			if(leftPosition <= infoWidth) {
+				// Close to the left edge of window, too!
+				// Calculate best fit.
+				if(windowWidth <= infoWidth) {
+					// WHY would your window be so small??
+					// Push it to the left edge. Hope we can scroll.
+					//	0 - (leftPosition - 2)
+					//	= 0 - leftPosition + 2
+					//	= 2 - leftPosition
+					let p = 2 - leftPosition;
+					s.transform = "translateX(" + p.toString() + "px)";
 				} else {
-					// To the left, to the left. Every tool you tip, in a box to the left.
-					info.css("transform", "translateX(" + (0 - infoWidth) + "px)");
+					// Moving it left 'infoWidth' pushes it off the left edge.
+					// Leaving it pushes it off the right edge by '(windowWidth - (leftPosition + infoWidth))' pixels.
+					// So move it leftward '(windowWidth - (leftPosition + infoWidth))' and add a bit of padding
+					// Should leave it just off the right edge!
+					//	windowWidth - (leftPosition + infoWidth)
+					//	= windowWidth - leftPosition - infoWidth
+					let p = windowWidth - leftPosition - infoWidth;
+					s.transform = "translateX(" + p.toString() + "px)";
 				}
+			} else {
+				// To the left, to the left. Every tool you tip, in a box to the left.
+				s.transform = "translateX(-" + infoWidth.toString() + "px)";
 			}
 		}
 	}
-});
+}
